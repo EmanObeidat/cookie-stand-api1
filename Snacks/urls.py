44c8e92de812c4de690f86af535d0bef737a9f54
eman obeidat <amoonobeidat43@gmail.com>
2023-08-05 14:13:29 +0000

    done

--- Snacks/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import SnackList, SnackDetail
-
-urlpatterns = [
-    path("", SnackList.as_view(), name="Snack_list"),
-    path("<int:pk>/", SnackDetail.as_view(), name="Snack_detail"),
-]
